11ed4c0c2ea10db2c455c07fc895a198800851a4
Unknown <khakhalin@gmail.com>
2018-11-10 16:41:43 -0500

    Towards hex-plotting of latencies

--- 2-Main analysis/caimg_experimens_assorted_figures.R
+++ 2-Main analysis/caimg_experimens_assorted_figures.R
@@ -9,8 +9,21 @@ require(ggplot2)
 
 rm(list = ls())  # Clear workspace
 
+# --- Network-level (brain-level) plots
 d <- read.table("7_Ca imaging/git - CaImaging Paper/2-Main analysis/caimg_network_summary.txt",
                 sep='\t',header=T)
 names(d)
 
+# Encoding, between stages:
 ggplot(d,aes(factor(Stage),encoding)) + theme_classic() + geom_point(shape=1)
+
+
+# --- All-cell plots
+d <- read.table("7_Ca imaging/git - CaImaging Paper/2-Main analysis/all_cells_latencies.csv",
+                sep=',',header=T)
+names(d)
+
+ds = d %>% group_by(ibrain) %>% summarize(minlat = quantile(lat,0.1))
+ggplot(d,aes(dist,lat)) + theme_classic() + geom_point(alpha=0.2)
+# Now subtract quantiles from each lat, and add average (~250) back
+# Then plut is as hex plot with alpha = density
